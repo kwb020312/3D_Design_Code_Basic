4042e1656dea6f476eda6a44f3b19298963ad80d
N S <kwb020312@naver.com>
2023-04-26 22:14:12 +0900

    Feat-Suspense 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import styled from "styled-components";
 
 import Background from "./components/Background";
@@ -5,6 +7,7 @@ import TextSection from "./components/TextSection";
 import Box from "./components/Box";
 
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function App() {
   return (
@@ -12,9 +15,12 @@ export default function App() {
       <Background />
       <TextSection />
       <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Box />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
       </Canvas>
     </Wrapper>
   );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,8 +1,14 @@
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import texture from "../assets/images/map.jpg";
+
 const Box = () => {
+  const colorMap = useLoader(TextureLoader, texture);
+
   return (
     <mesh rotation={[90, 0, 20]}>
       <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
-      <meshLambertMaterial attach="material" color={"blue"} />
+      <meshNormalMaterial attach={"material"} />
     </mesh>
   );
 };
